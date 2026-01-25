c1f1f646bb4fe576714019c8eb1a35c7778b185c
梧萱梦汐X <159620537+MKHkro1@users.noreply.github.com>
2026-01-26 01:39:47 +0800

    1

--- ToolModData/obj/Release/net6.0/ToolModData.AssemblyInfo.cs
+++ ToolModData/obj/Release/net6.0/ToolModData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToolModData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da4d8d4e1af5f656aae06e475e1d9867c580d7f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9eda36d5a3788ed9051ba275194848b532fcf0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToolModData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToolModData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
